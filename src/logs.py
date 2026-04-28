84990952aec877486a65f1565fec7355c2702195
Takumy <Talumy@duck.com>
2026-04-28 18:28:45 +0700

    logs are now cross platform

--- src/logs.py
+++ src/logs.py
@@ -1,10 +1,10 @@
 from datetime import datetime
 from pathlib import Path
-
+import logging
 
 LOGS_PATH = Path('./.logs')
-
 LOGS_PATH.mkdir(exist_ok=True)
 
-LOG_FILE = LOGS_PATH / \
-    (str(datetime.now().strftime('%d.%m.%Y-%H:%M:%S')) + '.log')
+LOG_FILE = LOGS_PATH / f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
+
+logging.basicConfig(filename=LOG_FILE, level=logging.DEBUG)
